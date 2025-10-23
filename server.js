1ed17aa1131b289ece0b72cf7f77b1b346ed35d5
Mahdi732 <ey449751@gmail.com>
2025-10-23 12:11:53 +0100

    creating server and app including ws

---
+++ server.js
@@ -0,0 +1,12 @@
+import app from "./src/app.js";
+import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`server is running on ${'http://localhost:' + port}`);
+})
---
+++ src/config/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default Class DataBase {
+    
+}
