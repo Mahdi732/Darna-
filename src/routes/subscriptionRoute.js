5762fc67350453221d4fb7c670b121f02bcc00c6
Safaa <Safaeettalhi@gmail.com>
2025-10-28 11:40:24 +0100

    creation de route subscription de contient les plant le subsraption

---
+++ src/routes/subscriptionRoute.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import PlanController from '../controllers/PlanController.js';
+import SubscriptionController from '../controllers/SubscriptionController.js';
+import { authenticateToken } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+const planController = new PlanController();
+const subscriptionController = new SubscriptionController();
+
+// Routes publiques
+router.get('/plans', planController.getAllPlans);
+router.get('/plans/:id', planController.getPlanById);
+router.post('/init-plans', planController.initDefaultPlans);
+
+// Routes protégées
+router.post('/subscribe', authenticateToken, subscriptionController.subscribe);
+router.get('/my-subscription', authenticateToken, subscriptionController.getMySubscription);
+router.delete('/cancel', authenticateToken, subscriptionController.cancelSubscription);
+
+export default router;
