3903fade460476b28ccd7a153bf8494721ae3f6a
Safaa <Safaeettalhi@gmail.com>
2025-10-28 09:00:16 +0100

    simplification

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 import App from './src/app.js';
 
-const app = new App();
-app.start();
+async function startServer() {
+    const app = new App();
+    
+    try {
+        await app.start();  
+        console.log('Serveur complètement démarré et prêt !');
+        
+    } catch (error) {
+        console.error('Erreur lors du démarrage:', error);
+        process.exit(1);
+    }
+}
+
+startServer();
